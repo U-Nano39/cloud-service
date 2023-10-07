3e6f5350127d3d7fbff1c7bddab0ffc89b260115
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-10-07 11:00:17 +0900

    Update JustAlive.py

--- JustAlive.py
+++ JustAlive.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask("")
 
 @app.route("/")
-def home():
-    return "<h1>Working.</h1><br /><p>その他情報: 44Ki44Kr44Km44Oz44OI44Ot44Kw44Kk44Oz5pa55rOVIC0gUmVuZGVyOiBHaXRIdWIgLyBVcHRpbWVSb2JvdDogW0VtYWlsXSByaWtpMDQyNTBAZ21haWwuY29tIFtQYXNzd29yZF0gcmlraTA0MjU=</p>"
+def index():
+    return render_template("index.html")
 
 def run():
     app.run(host="0.0.0.0", port="8000")
