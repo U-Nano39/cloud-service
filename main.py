21b660ed42359374d54756a40eb03bcd6c86b225
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-10-07 20:45:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ class Discord:
     def render_shell(self):
         while True:
             if self.message_content().startswith("r#cmd"):
-                cmd = self.message_content().strip("r#cmd ")
-                proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                cmd = self.message_content().strip("r#cmd ").split(" ")
+                proc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 cmd_r = " ".join(cmd)
                 self.send_message(f"``[cmd] {cmd_r}``\n```{proc.communicate()[0].decode('UTF-8')}```")
                 
