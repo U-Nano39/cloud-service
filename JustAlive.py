51ae776c4d8b59bf185cdbb0dc7997674ef42ad6
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-10-07 11:35:19 +0900

    Update JustAlive.py

--- JustAlive.py
+++ JustAlive.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from threading import Thread
 
-app = Flask("")
+app = Flask(__name__)
 
-@app.route("/SelfBot")
+@app.route("/")
 def index():
     return render_template("index.html")
 
