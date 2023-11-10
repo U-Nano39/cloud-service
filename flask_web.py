872824418a3a69b3abbdde70e74bd832ea642f01
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-11-11 01:19:39 +0900

    Update flask_web.py

--- flask_web.py
+++ flask_web.py
@@ -17,8 +17,9 @@ def favicon(avatar):
             "User-Agent": "Discord SelfBot Python-urllib/3.9",
             "Authorization": BotKey
             }
+    print(f"https://discord.com/api/v9/users/441865412804870144/{avatar}.jpg")
     with open("static/images/favicon.jpg", "wb") as fvi:
-        fvi.write(urlopen(Request(f"https://discord.com/api/v9/users/441865412804870144/{avatar}", headers=headers)).read())
+        fvi.write(urlopen(Request(f"https://discord.com/api/v9/users/441865412804870144/{avatar}.jpg", headers=headers)).read())
         fvi.close()
     
     favicon = Image.open("static/images/favicon.jpg")
