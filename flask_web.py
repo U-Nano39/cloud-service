30c9fcc0c5f6fdcb00ac44c88505efeca97f05b0
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-11-10 23:58:29 +0900

    Update flask_web.py

--- flask_web.py
+++ flask_web.py
@@ -1,7 +1,20 @@
+from PIL import Image
+
+from urllib.request import Request, urlopen
 from flask import Flask, render_template
 from threading import Thread
 
 app = Flask(__name__)
+
+#favicon.ico
+def favicon(avatar):
+    with open("favicon.jpg", "wb") as fvi:
+        fvi.write(urlopen(f"https://cdn.discordapp.com/avatars/441865412804870144/{avatar}").read())
+        fvi.close()
+        
+    favicon = Image.open("favicon.jpg")
+    favicon.save("static/favicon.ico")
+    
 
 @app.route("/")
 def index():
@@ -15,5 +28,7 @@ def run():
     app.run(host="0.0.0.0", port="8000")
 
 def Elphelt():
+    favicon()
+    
     thread = Thread(target=run)
     thread.start()
