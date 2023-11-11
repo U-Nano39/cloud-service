f20596dae525ce7d1f266b0f329bec5e3887c864
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-11-11 11:21:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ channel_id = [798566757101994015, 1114531313584709684]
 #channel id list memo 0:main server 1:namek free channel
 time_list = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"]
 
-BotKey = os.getenv("DISCORD_BOT_TOKEN") #os.getenv("DISCORD_USER_TOKEN")
+Bot = os.getenv("DISCORD_BOT_TOKEN")
+User = os.getenv("DISCORD_USER_TOKEN")
+
+BotKey = Bot
 
 class Discord:
     def __init__(self, token) -> None:
@@ -24,9 +27,9 @@ class Discord:
             "User-Agent": "Discord SelfBot Python-urllib/3.9",
             }
 
-        if BotKey == os.getenv("DISCORD_BOT_TOKEN"):
+        if BotKey == Bot:
             self.headers["Authorization"] = f"Bot {self.token}"
-        elif BotKey == os.getenv("DISCORD_USER_TOKEN"):
+        elif BotKey == User:
             self.headers["Authorization"] = self.token
 
     def on_ready(self, msg):
