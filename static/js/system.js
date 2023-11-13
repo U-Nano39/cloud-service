7d35f62837d780a0ef412ac5450e499b6d1ccee0
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-11-13 12:59:21 +0900

    Update system.js

--- static/js/system.js
+++ static/js/system.js
@@ -1,1 +1,11 @@
 console.log("Welcome to Qvey's Website.")
+
+const ul0 = document.querySelector("#list0")
+
+ul0.addEventListener("mouseover", function() {
+    ul0.style.textDecoration = "underline";
+});
+
+ul0.addEventListener("mouseleave", function() {
+    ul0.style.textDecoration = "none";
+})
