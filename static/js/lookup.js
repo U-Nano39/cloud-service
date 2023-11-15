63ffc8c01111e26aa63c6905fc0d56543bc0c5b7
U-Nano39 <73567618+U-Nano39@users.noreply.github.com>
2023-11-15 11:38:59 +0900

    Update lookup.js

--- static/js/lookup.js
+++ static/js/lookup.js
@@ -6,6 +6,13 @@ window.addEventListener("DOMContentLoaded", function() {
     const InputArea = document.querySelector("#InputArea");
     const search = document.querySelector("#search");
 
+    InputArea.onkeypress = function(event){
+        if (event.key === 'Enter') {
+            const ID = InputArea.value;
+            location.href = ID;
+            };
+        };
+
     search.addEventListener("click", function() {
         const ID = InputArea.value;
         location.href = ID;
